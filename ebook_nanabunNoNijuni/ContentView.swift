779653bb27ae91b0e206d818e3ad8befebff360b
Gordon Chou <chouguting@gmail.com>
2021-11-04 17:29:05 +0800

    Initial Commit

---
+++ ebook_nanabunNoNijuni/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  ebook_nanabunNoNijuni
+//
+//  Created by 周固廷 on 2021/11/4.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ ebook_nanabunNoNijuni/ebook_nanabunNoNijuniApp.swift
@@ -0,0 +1,17 @@
+//
+//  ebook_nanabunNoNijuniApp.swift
+//  ebook_nanabunNoNijuni
+//
+//  Created by 周固廷 on 2021/11/4.
+//
+
+import SwiftUI
+
+@main
+struct ebook_nanabunNoNijuniApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
